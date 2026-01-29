0ff4d6b34abc74240700201aae1e85fa6e2ab12e
harjit90 <mehmi.harjit@gmail.com>
2026-01-29 20:54:28 +0530

    Updating source code

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
+// export default defineConfig({
+//   plugins: [react()],
+//   base: '/marketify/',
+// })
+
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  base: '/marketify/',
-})
+  base: mode === 'github' ? '/marketify/' : '/',
+}))
